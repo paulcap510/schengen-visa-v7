58e8cbeb2c7e9dddde49b4baa5fd11fcf01e1d5a
Paul <paulcapob@gmail.com>
2024-04-03 10:15:46 -0400

    Added absolute path to correct routing

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,33 @@ const cors = require('cors');
 const app = express();
 const helmet = require('helmet');
 const port = process.env.PORT || 3000;
+const path = require('path');
 
 app.use(helmet());
 app.use(cors());
 
 console.log(`Current working directory: ${process.cwd()}`);
 
-const countryData = require('../frontend/src/country_data_updated.json');
-const safetyData = require('../frontend/src/safety_data.json');
+const safetyDataPath = path.join(
+  __dirname,
+  '..',
+  '..',
+  'frontend',
+  'src',
+  'safety_data.json'
+);
+const safetyData = require(safetyDataPath);
+const countryDataPath = path.join(
+  __dirname,
+  '..',
+  '..',
+  'frontend',
+  'src',
+  'country_data_updated.json'
+);
+const countryData = require(countryDataPath);
+// const countryData = require('../frontend/src/country_data_updated.json');
+// const safetyData = require('../frontend/src/safety_data.json');
 
 app.get('/api/countrydata', (req, res) => {
   res.json(countryData);
